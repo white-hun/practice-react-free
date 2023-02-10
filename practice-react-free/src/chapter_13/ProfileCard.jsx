36c711f55b2daffebc4d80d48d4d2c895208882c
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-11 01:35:18 +0900

    Update practice rf

---
+++ practice-react-free/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <div>
+            <Card title="Spacer White" backgroundColor="">
+                <p>I'm Shadow Master</p>
+                <p>I'm studying with React</p>
+            </Card>
+        </div>
+    );
+}
+
+export default ProfileCard;
--- practice-react-free/src/index.js
+++ practice-react-free/src/index.js
@@ -14,6 +14,7 @@ import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
+import ProfileCard from "./chapter_13/ProfileCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -27,7 +28,8 @@ root.render(
   // <LandingPage />
   // <AttendanceBook />
   // <SignUp />
-  <Calculator />
+  // <Calculator />
+  <ProfileCard />
   // </React.StrictMode>
 );
 
